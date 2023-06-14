9ff96534ac9b29cc20ee3ac65eb0e9e7338381e9
Clément Dubreuil <clement@dubreuil.dev>
2023-06-15 01:12:38 +0200

    Add github webhook

--- clusters/maktha/terraform/github.tf
+++ clusters/maktha/terraform/github.tf
@@ -4,3 +4,15 @@ resource "github_repository_deploy_key" "fleet-infra" {
   key        = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGINc2v1YVfuWo+36jVR6wUkJqGIMfHt6x7ekBgrkv2Z"
   read_only  = true
 }
+
+resource "github_repository_webhook" "fleet-infra" {
+  repository = "fleet-infra"
+  events     = ["push"]
+
+  configuration {
+    url          = var.fleet_infra_webhook_url
+    content_type = "json"
+    secret       = var.fleet_infra_webhook_secret
+    insecure_ssl = false
+  }
+}
--- clusters/maktha/terraform/variables.tf
+++ clusters/maktha/terraform/variables.tf
@@ -1,7 +1,19 @@
 variable "api_floating_ip_api_token" {
-  type = string
+  type      = string
+  sensitive = true
 }
 
 variable "github_token" {
-  type = string
+  type      = string
+  sensitive = true
+}
+
+variable "fleet_infra_webhook_url" {
+  type      = string
+  sensitive = true
+}
+
+variable "fleet_infra_webhook_secret" {
+  type      = string
+  sensitive = true
 }
